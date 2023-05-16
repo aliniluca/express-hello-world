7bd1f2fe420af62085a27d7c94ad20a4607af27a
aliniluca <115418094+aliniluca@users.noreply.github.com>
2023-05-16 16:53:30 +0300

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  const dropdown = document.getElementById('readingTypeDropdown');
+  const buttonContainer = document.getElementById('buttonContainer');
+  const output = document.getElementById('output');
+
+  // Add 30 buttons
+  for (let i = 1; i <= 30; i++) {
+    const button = document.createElement('button');
+    button.textContent = i;
+    button.addEventListener('click', () => getFile(dropdown.value, i));
+    buttonContainer.appendChild(button);
+  }
+
+  // Fetch a file from the server and update the textarea
+  function getFile(readingType, number) {
+    fetch(`http://localhost:3000/get-file?readingType=${readingType}&number=${number}`)
+      .then(response => response.text())
+      .then(text => output.value = text)
+      .catch(err => console.error(err));
+  }
+};
