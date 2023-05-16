491afeb3123b6f914621719ad8403094d9735fa0
aliniluca <115418094+aliniluca@users.noreply.github.com>
2023-05-16 21:09:00 +0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -2,7 +2,24 @@ window.onload = function() {
   const dropdown = document.getElementById('readingTypeDropdown');
   const buttonContainer = document.getElementById('buttonContainer');
   const output = document.getElementById('output');
+  const copyButton = document.getElementById('copyButton');
 
+  // Add 30 buttons
+  for (let i = 1; i <= 30; i++) {
+    const button = document.createElement('button');
+    button.textContent = i;
+    button.addEventListener('click', () => getFile(dropdown.value, i));
+    buttonContainer.appendChild(button);
+  }
+
+  // Copy text to clipboard
+  copyButton.addEventListener('click', () => {
+    navigator.clipboard.writeText(output.value).then(() => {
+      alert('Text copied to clipboard');
+    }).catch(err => {
+      console.error('Could not copy text: ', err);
+    });
+  });
   // Add 30 buttons
   for (let i = 1; i <= 30; i++) {
     const button = document.createElement('button');
