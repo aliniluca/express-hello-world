edc15cac25f1438f762246c4eebf1aa54bf76134
aliniluca <115418094+aliniluca@users.noreply.github.com>
2023-05-17 15:37:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const AWS = require('aws-sdk');
 const cors = require('cors');
-const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const s3 = new AWS.S3();
 
 app.use(cors());
-
-// Serve static files from the "public" directory
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.use(bodyParser.json()); // for parsing application/json
 
 app.get('/get-file', async (req, res) => {
   const { readingType, number } = req.query;
@@ -26,6 +20,24 @@ app.get('/get-file', async (req, res) => {
   try {
     const data = await s3.getObject(params).promise();
     res.send(data.Body.toString());
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('An error occurred');
+  }
+});
+
+app.post('/edit-and-save-file', async (req, res) => {
+  const { readingType, number, newText } = req.body;
+
+  const params = {
+    Bucket: 'cyclic-calm-gold-millipede-tie-sa-east-1',
+    Key: `${readingType}/${number}.txt`,
+    Body: newText,
+  };
+
+  try {
+    await s3.putObject(params).promise();
+    res.send('File updated successfully');
   } catch (err) {
     console.error(err);
     res.status(500).send('An error occurred');
